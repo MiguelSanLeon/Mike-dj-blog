956aaf89ea10169e32d9bb723386c20762e2d15f
MiguelSanLeon <>
2023-10-25 10:47:49 +0000

    Fix ALLOWED_HOST on settings.py

--- codestart/settings.py
+++ codestart/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant-booking-app-mike.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['restaurant-booking-app-mike-473ff28d1478.herokuapp.com', 'localhost']
 
 
 # Application definition
